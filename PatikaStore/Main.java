9d2cada87faccfb4a650ba96d39005200516e886
nthY <n_hat.yalcin@outlook.com>
2022-09-19 21:31:40 +0300

    delete comment lines in Main.java

--- PatikaStore/Main.java
+++ PatikaStore/Main.java
@@ -2,31 +2,9 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		PatikaStore store = new PatikaStore();
 		store.listMainOperations();
-		/*
-		Brand.listBrands();
-		System.out.println(Product.addCategory("Notebook"));
-		Product.productCategories.forEach((key, value) -> System.out.println(key + " " + value.isEmpty()));
-		
-		Product samsungPhone = new MobilePhone(Product.getID("Mobile phone"), 1200.0, 10, 15, "Galaxy", Brand.getBrand("ASD"), 3, 64, 5.1);
-		Product samsungPhone2 = new MobilePhone(Product.getID("Mobile phone"), 1200.0, 10, 15, "Galaxy-2", Brand.getBrand("Samsung"), 3, 64, 5.1);
-		Product HP = new Notebook(Product.getID("Notebook"), 1200.0, 10, 15, "HP notebook", Brand.getBrand("HP"), 3, 64, 5.1);
-		
-		
-		Product.addProduct("Mobile phone", samsungPhone);
-		Product.addProduct("Notebook", HP);
-		Product.addProduct("Mobile phone", samsungPhone2);
-		
-		Product.productCategories.forEach((key, value) -> {
-			System.out.print("**" + key + "**\n");
-			value.forEach(p -> System.out.println(p.getBrand().getName() + ", " + p.getName() + ", " + p.getId()));
-		});
-		
-
-		Product.productCategories.forEach((key, value) -> System.out.println(key + " " + value.isEmpty()));
-		*/
 
 	}
 

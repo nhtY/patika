b5b592118eaca12cdc56f915732211593b2ee6ba
nthY <n_hat.yalcin@outlook.com>
2022-11-04 21:04:27 +0300

    init odev2

---
+++ src/odev2/GreenGroceryCashRegister.java
@@ -0,0 +1,21 @@
+
+/**
+* The GreenGroceryCashRegister program implements an application that
+* simply calculates the amount of money that has to be taken from the
+* customer in return for what is sold in a grocery store.
+*
+* @author  Nihat Yalcin
+*/
+
+package odev2;
+
+public class GreenGroceryCashRegister {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

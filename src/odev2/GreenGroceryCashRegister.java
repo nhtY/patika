e5929a5db107dc81a0988bad07c64346de8db30a
nthY <n_hat.yalcin@outlook.com>
2022-11-04 21:36:11 +0300

    calculate the amount of money using for loop

--- src/odev2/GreenGroceryCashRegister.java
+++ src/odev2/GreenGroceryCashRegister.java
@@ -1,6 +1,6 @@
 
 /**
-* The GreenGroceryCashRegister program implements an application that
+* The GreenGroceryCashRegister program
 * simply calculates the amount of money that has to be taken from the
 * customer in return for what is sold in a grocery store.
 *
@@ -9,12 +9,51 @@
 
 package odev2;
 
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+
 public class GreenGroceryCashRegister {
-	
 	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// initialize a LinkedHashMap to keep Product-Price pairs
+		LinkedHashMap<String, Double> products = new LinkedHashMap<>();
+		
+		// put the pairs into the linked hash map
+		products.put("Armut", 2.14);
+		products.put("Elma", 3.67);
+		products.put("Domates", 1.11);
+		products.put("Muz", 0.95);
+		products.put("Patlıcan", 5.00);
+		
+		// create a Scanner object to take user input
+		Scanner scanner = new Scanner(System.in);
+		
+		// to keep the user input indicating kilogram of product:
+		double amountOfProduct;
+		
+		// price of the product
+		double price;
+		
+		// to keep the amount of money to take from customer:
+		double amountOfMoney = 0;
+		
+		// iterate through each pair and ask user how much s/he wants to buy
+		for(String key : products.keySet()) {
+			System.out.printf("%s Kaç Kilo? :", key);
+			amountOfProduct = scanner.nextDouble();
+			
+			price = products.get(key);
+			amountOfMoney += amountOfProduct * price; 
+			
+		}
+		
+		// close the scanner
+		scanner.close();
+		
+		// print out the total amount of money
+		System.out.printf("Toplam Tutar: %.2f TL\n", amountOfMoney);
 
 	}
 

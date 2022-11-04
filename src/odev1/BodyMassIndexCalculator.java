807a05d538a111bc2c5066d25e614a6e026e1aee
nthY <n_hat.yalcin@outlook.com>
2022-11-04 18:09:35 +0300

    write a method to calculate BMI and test it

--- src/odev1/BodyMassIndexCalculator.java
+++ src/odev1/BodyMassIndexCalculator.java
@@ -1,10 +1,52 @@
+
+/**
+* The BodyMassIndexCalculator program implements an application that
+* simply calculates body mass index with respect to given formula.
+* Also, it displays the result to the console.
+*
+* @author  Nihat Yalcin
+*/
+
+
 package odev1;
+
+import java.util.Scanner;
 
 public class BodyMassIndexCalculator {
 
+	/**
+	 * This method calculates BMI using the following formula:
+	 * weight / (height * height)
+	 * and returns a double result.
+	 * @param height
+	 * @param weight
+	 * @return double result
+	 */
+	public static double calculateBMI(double height, double weight) {
+		return weight / (height * height);
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// take user input
+		Scanner scanner = new Scanner(System.in);
+		
+		// take the height input in meter
+		System.out.printf("Lütfen boyunuzu metre cinsinden giriniz: ");
+		double height = scanner.nextDouble();
+		
+		// take the weight input
+		System.out.printf("Lütfen kilonuzu giriniz: ");
+		double weight = scanner.nextDouble();
+		
+		// close the scanner
+		scanner.close();
+		
+		// Let's use the calculateBMI method:
+		System.out.printf("Vücut Kitle İndeksiniz: %f\n", calculateBMI(height, weight));
 
 	}
+	
+	
 
 }

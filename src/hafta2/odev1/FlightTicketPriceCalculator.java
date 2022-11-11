97a136312e0d0c14458895716abdeb51a388e2b1
nthY <n_hat.yalcin@outlook.com>
2022-11-11 21:07:40 +0300

    create a java file, add explanation of the program

---
+++ src/hafta2/odev1/FlightTicketPriceCalculator.java
@@ -0,0 +1,20 @@
+
+/**
+* The FlightTicketPriceCalculator program 
+* simply calculates ticket prices of a flight 
+* according to the age of the passenger, distance, type of the trip etc.
+* Also, it displays the result to the console.
+*
+* @author  Nihat Yalcin
+*/
+
+package hafta2.odev1;
+
+public class FlightTicketPriceCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
